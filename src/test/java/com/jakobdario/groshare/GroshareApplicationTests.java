2f2f6f136ac1b20363e226d68d343fe7e0df01c5
Dario Stromajer <stromajer.dario@gmail.com>
2026-03-15 13:14:34 +0100

    Initialize spring project with hello world api

---
+++ src/test/java/com/jakobdario/groshare/GroshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakobdario.groshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
