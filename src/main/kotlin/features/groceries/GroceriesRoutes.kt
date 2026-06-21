04c8124bbedd44bca65592b0d4339ee2d2a08869
Dario Stromajer <115354821+DarioLoll@users.noreply.github.com>
2026-06-21 22:18:51 +0200

    Implemented buying groceries for debt tracking

--- src/main/kotlin/features/groceries/GroceriesRoutes.kt
+++ src/main/kotlin/features/groceries/GroceriesRoutes.kt
@@ -1,6 +1,7 @@
 package dev.jakobdario.features.groceries
 
 import dev.jakobdario.database.Database
+import dev.jakobdario.features.payments.buyGroceries
 import io.ktor.server.routing.*
 
 fun Route.groceriesRoutes(database: Database) {
---
+++ src/main/kotlin/features/payments/PaymentRoutes.kt
@@ -0,0 +1,8 @@
+package dev.jakobdario.features.payments
+
+import dev.jakobdario.database.Database
+import io.ktor.server.routing.Route
+
+fun Route.paymentRoutes(database: Database) {
+    buyGroceries(database)
+}
